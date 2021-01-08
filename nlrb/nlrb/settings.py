3178ac4147ee5f0a8b489ed64a606140cfdec92b
dlyons33 <dennis.lyons4@gmail.com>
2021-01-08 07:19:59 -0500

    secret key to env var

--- nlrb/nlrb/settings.py
+++ nlrb/nlrb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yf0twb=i=meugad0_z%%&&@%(1e#2gifab-lazsys%k-1&mmat'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
